71cabc72b4b1f4b813adb5811c1e4da717567fd6
ahmedbda <prsmul@pm.me>
2025-12-21 10:27:13 +0100

    removing api key from json req result

--- main.py
+++ main.py
@@ -15,17 +15,36 @@ app = FastAPI()
 load_dotenv() # loads .env (api key) needed since this project will be open source on github
 api_key = os.getenv("nasa_api_key")
 
-# timestamps calculation
-today = datetime.now().strftime("%Y-%m-%d")
-
-# api request for today
-request = requests.get(f"https://api.nasa.gov/neo/rest/v1/feed?start_date={today}&end_date={today}&api_key={api_key}")
-
-#test
-print(request)
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
+# loading html site (frontend)
 @app.get("/", response_class=HTMLResponse)
 def main_site():
     return FileResponse("static/index.html")
+
+@app.get("/asteroids")
+def get_asteroids_data():
+    # timestamps calculation
+    today = datetime.now().strftime("%Y-%m-%d")
+    
+    # request url (separated for readability) 
+    url = f"https://api.nasa.gov/neo/rest/v1/feed?start_date={today}&end_date={today}&api_key={api_key}"
+
+
+    # api request for today and extracting the json from it in data
+    data = requests.get(url).json()
+
+    # removing api key from top level links for security purposes
+    if "links" in data:
+        del data["links"]
+
+    # removing api key from full json file
+    near_earth_objects = data.get("near_earth_objects", {})    
+    for date in near_earth_objects:
+        for asteroid in near_earth_objects[date]:
+            if "links" in asteroid:
+                del asteroid["links"]
+
+    # checking response json file
+    return data
+
+# serving the static app
+app.mount("/static", StaticFiles(directory="static"), name="static")
